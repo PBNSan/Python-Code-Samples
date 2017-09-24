bb30cf9b6b18bd7730aaeb6bdf45301b8aa031a8
Preethi <drp2preet@gmail.com>
2017-09-24 20:25:45 +0530

    Added a new Code Sample for Calculating surface area of a solid and hollow cylinder

---
+++ cylinder_solid_hollow.py
@@ -0,0 +1,13 @@
+# The cylinder_surface_area function calculates the surface area of cylinders that are solid or hollow. The has_top_and_bottom argument is True or False depending on whether the cylinder is solid or hollow. The surface area of a solid cylinder includes the areas of the top and bottom
+# Restructure this function definition so that it has two return statements in its body.
+
+def cylinder_surface_area(radius, height, has_top_and_bottom):
+    side_area = height * 6.28 * radius
+    if has_top_and_bottom:
+        top_area = 3.14 * radius ** 2
+        side_area += 2 * top_area
+        return side_area
+    if not has_top_and_bottom:
+    	return side_area
+  
+  
