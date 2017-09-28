5e5fe0f826f54a60f4cf1f1df184e3dbf8a66472
Preethi <drp2preet@gmail.com>
2017-09-28 13:02:27 +0530

    Added an alternative approach to nearest square function

--- nearest_square.py
+++ nearest_square.py
@@ -2,19 +2,27 @@
 # There's more than one way to write this code, but I suggest you use a while loop!
 
 def nearest_square(limit):
+
 	"""
 	This function takes a limit, finds a square value closest to the limit and returns the square value.
 	"""
-	i = 1
-	bef_limit = 1
-	aft_limit = 1
+	#Approach 1
+	# i = 1
+	# bef_limit = 1
+	# aft_limit = 1
 
-	while  aft_limit < limit:
-		bef_limit = i * i
-		i += 1
-		aft_limit = i * i
+	# while  aft_limit < limit:
+	# 	bef_limit = i * i
+	# 	i += 1
+	# 	aft_limit = i * i
 
-	return print(bef_limit)
+	# return print(bef_limit)
 
+	#Approach 2
+	answer = 0
+	while (answer + 1) ** 2 < limit:
+		answer += 1
+	return print(answer **2)
 
-nearest_square(56)
+nearest_square(40)
+
